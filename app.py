a95170db8bcbf324c57093d9abcb544d17341bac
ThePreatorian9 <122523486+ThePreatorian9@users.noreply.github.com>
2023-01-18 01:35:17 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,20 @@
+
 from flask import Flask
-import os
+from prometheus_client import make_wsgi_app
+from werkzeug.middleware.dispatcher import DispatcherMiddleware
+from werkzeug.serving import run_simple
+from flask_prometheus_metrics import register_metrics
 
 app = Flask(__name__)
 
 @app.route("/")
-def hello():
-    return "Flask inside Docker!!"
+def index():
+  return "Test"
 
+# provide app's version and deploy environment/config name to set a gauge metric
+register_metrics(app, app_version="v0.1.2", app_config="staging")
 
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(debug=True,host='0.0.0.0',port=port)
+# Plug metrics WSGI app to your main app with dispatcher
+dispatcher = DispatcherMiddleware(app.wsgi_app, {"/metrics": make_wsgi_app()})
+
+run_simple(hostname="0.0.0.0", port=5000, application=dispatcher
